ef70a8c2956bcd7cab1a1605ff2d420372d711ae
robert.jedrzejewski <robert.jedrzejewski@dev-chop-chop.org>
2019-04-05 16:02:36 +0200

    Add Apollo and GraphQL packages / Init ApolloClient and Provider

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { ApolloProvider } from 'react-apollo';
 
 import Routes from 'components/Routes';
 
+import { client as ApolloClient } from 'services/Apollo';
+
 const App = () => (
-    <Routes />
+    <ApolloProvider client={ApolloClient}>
+        <Routes />
+    </ApolloProvider>
 );
 
 export default App;
