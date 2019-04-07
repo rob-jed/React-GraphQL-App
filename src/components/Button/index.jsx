5493d32ca0936683f547edca9ec8acf165199de6
robert.jedrzejewski <robert.jedrzejewski@dev-chop-chop.org>
2019-04-07 14:42:25 +0200

    Add option to insert customClass to buttons

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,21 +1,34 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+import { classNames } from 'services/Styles';
+
 const propTypes = {
     text: PropTypes.string.isRequired,
     onClick: PropTypes.func.isRequired,
+    customClass: PropTypes.string,
 };
 
-const Button = ({ text, onClick }) => (
+const defaultProps = {
+    customClass: '',
+};
+
+const Button = ({ text, onClick, customClass }) => (
     <button
         type="button"
         onClick={onClick}
-        className="btn"
+        className={
+            classNames(
+                'btn',
+                customClass,
+            )
+        }
     >
         { text }
     </button>
 );
 
 Button.propTypes = propTypes;
+Button.defaultProps = defaultProps;
 
 export default Button;
